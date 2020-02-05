3afde054193b0fcb78f66084e7bccfc4ea63aecc
Rem Lampa <rem@remlampa.com>
2020-02-05 22:01:45 +0800

    Add snapshot for QueryField :camera_flash:

--- src/__tests__/snapshot/index.spec.js
+++ src/__tests__/snapshot/index.spec.js
@@ -2,12 +2,30 @@ import React from "react";
 import { shallow } from "enzyme";
 import toJson from "enzyme-to-json";
 
-import SearchForm from "../../components/SearchForm";
+import SearchForm, { QueryField } from "../../components/SearchForm";
 
 describe("SNAPSHOT TESTS", () => {
   describe("SearchForm Component", () => {
     it("should render properly", () => {
       const element = shallow(<SearchForm />);
+
+      expect(toJson(element)).toMatchSnapshot();
+    });
+  });
+
+  describe("QueryField Component", () => {
+    it("should render properly", () => {
+      const field = {
+        id: "some-id",
+        name: "some-name"
+      };
+
+      const props = {
+        required: true,
+        onSubmit: () => {}
+      };
+
+      const element = shallow(<QueryField field={field} {...props} />);
 
       expect(toJson(element)).toMatchSnapshot();
     });

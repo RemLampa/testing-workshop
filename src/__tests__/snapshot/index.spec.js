e9c91602400cf56fee24fab5897754220b8df27f
Rem Lampa <rem@remlampa.com>
2020-02-06 00:24:36 +0800

    Add snapshot for SearchForm submission :camera_flash:

--- src/__tests__/snapshot/index.spec.js
+++ src/__tests__/snapshot/index.spec.js
@@ -1,6 +1,10 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { act } from "react-dom/test-utils";
+import { shallow, mount } from "enzyme";
 import toJson from "enzyme-to-json";
+
+import axios from "axios";
+import faker from "faker";
 
 import SearchForm, {
   QueryField,
@@ -8,12 +12,101 @@ import SearchForm, {
   OrderSelect
 } from "../../components/SearchForm";
 
+faker.seed(824);
+
+const items = [
+  {
+    id: faker.random.uuid(),
+    name: faker.name.findName(),
+    owner: {
+      avatar_url: faker.image.avatar()
+    },
+    stargazers_count: faker.random.number(10000),
+    forks_count: faker.random.number(10000),
+    open_issues_count: faker.random.number(1000),
+    updated_at: faker.date.between("2019-01-01", "2019-12-31").toString()
+  },
+  {
+    id: faker.random.uuid(),
+    name: faker.name.findName(),
+    owner: {
+      avatar_url: faker.image.avatar()
+    },
+    stargazers_count: faker.random.number(10000),
+    forks_count: faker.random.number(10000),
+    open_issues_count: faker.random.number(1000),
+    updated_at: faker.date.between("2019-01-01", "2019-12-31").toString()
+  },
+  {
+    id: faker.random.uuid(),
+    name: faker.name.findName(),
+    owner: {
+      avatar_url: faker.image.avatar()
+    },
+    stargazers_count: faker.random.number(10000),
+    forks_count: faker.random.number(10000),
+    open_issues_count: faker.random.number(1000),
+    updated_at: faker.date.between("2019-01-01", "2019-12-31").toString()
+  }
+];
+
+axios.get.mockResolvedValue({
+  data: { items }
+});
+
 describe("SNAPSHOT TESTS", () => {
   describe("SearchForm Component", () => {
     it("should render properly", () => {
       const element = shallow(<SearchForm />);
 
       expect(toJson(element)).toMatchSnapshot();
+    });
+
+    it("should render repositories upon form submission", async () => {
+      const wrapper = mount(<SearchForm />);
+
+      const testQuery = faker.random.word();
+      wrapper
+        .find(QueryField)
+        .find("input")
+        .simulate("change", {
+          target: {
+            name: "query",
+            value: testQuery
+          }
+        });
+
+      const testSort = "forks";
+      wrapper
+        .find(SortSelect)
+        .find("select")
+        .simulate("change", {
+          target: {
+            name: "sort",
+            value: testSort
+          }
+        });
+
+      const testOrder = "asc";
+      wrapper
+        .find(OrderSelect)
+        .find("select")
+        .simulate("change", {
+          target: {
+            name: "order",
+            value: testOrder
+          }
+        });
+
+      wrapper.update();
+      expect(wrapper.find(OrderSelect));
+
+      await act(async () => {
+        wrapper.find("form").simulate("submit");
+      });
+
+      wrapper.update();
+      expect(toJson(wrapper)).toMatchSnapshot();
     });
   });
 

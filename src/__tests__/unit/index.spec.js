738613078c204ad256ea431db1257634d7ba7639
Rem Lampa <rem@remlampa.com>
2020-02-06 23:11:37 +0800

    Add test for Sort Select label :white_check_mark:

--- src/__tests__/unit/index.spec.js
+++ src/__tests__/unit/index.spec.js
@@ -3,7 +3,7 @@ import { shallow } from "enzyme";
 import { Label, Input } from "@rebass/forms";
 import faker from "faker";
 
-import { QueryField } from "../../components/SearchForm";
+import { QueryField, SortSelect } from "../../components/SearchForm";
 
 faker.seed(824);
 
@@ -35,6 +35,26 @@ describe("UNIT TESTS", () => {
       expect(input.exists()).toBeTruthy();
       expect(input).toHaveLength(1);
       expect(input.prop("fieldProp")).toEqual(props.field.fieldProp);
+    });
+  });
+
+  describe("SortSelect Component", () => {
+    const props = {
+      field: {
+        fieldProp: faker.random.words()
+      },
+      testProp: faker.random.words()
+    };
+
+    it("should render a proper label", () => {
+      const wrapper = shallow(<SortSelect {...props} />);
+
+      const label = wrapper.find(Label);
+
+      expect(label.exists()).toBeTruthy();
+      expect(label).toHaveLength(1);
+      expect(label.prop("htmlFor")).toBe("sort");
+      expect(label.text()).toBe("Sort");
     });
   });
 });

9e668f48781eb64a55ede53769a07b2fb272b56c
Rem Lampa <rem@remlampa.com>
2020-02-15 16:37:57 +0800

    Add integration test for search form rendering :white_check_mark:

--- src/__tests__/integration/index.spec.js
+++ src/__tests__/integration/index.spec.js
@@ -1,7 +1,51 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import faker from "faker";
+import axios from "axios";
 
 import SearchForm from "../../components/SearchForm";
+
+faker.seed(824);
+
+const items = [
+  {
+    id: faker.random.uuid(),
+    name: faker.name.findName(),
+    owner: {
+      avatar_url: faker.image.avatar()
+    },
+    stargazers_count: faker.random.number(10000),
+    forks_count: faker.random.number(10000),
+    open_issues_count: faker.random.number(1000),
+    updated_at: faker.date.between("2019-01-01", "2019-12-31").toString()
+  },
+  {
+    id: faker.random.uuid(),
+    name: faker.name.findName(),
+    owner: {
+      avatar_url: faker.image.avatar()
+    },
+    stargazers_count: faker.random.number(10000),
+    forks_count: faker.random.number(10000),
+    open_issues_count: faker.random.number(1000),
+    updated_at: faker.date.between("2019-01-01", "2019-12-31").toString()
+  },
+  {
+    id: faker.random.uuid(),
+    name: faker.name.findName(),
+    owner: {
+      avatar_url: faker.image.avatar()
+    },
+    stargazers_count: faker.random.number(10000),
+    forks_count: faker.random.number(10000),
+    open_issues_count: faker.random.number(1000),
+    updated_at: faker.date.between("2019-01-01", "2019-12-31").toString()
+  }
+];
+
+axios.get.mockResolvedValue({
+  data: { items }
+});
 
 describe("INTEGRATION TESTS", () => {
   describe("SearchForm Component", () => {
@@ -13,6 +57,23 @@ describe("INTEGRATION TESTS", () => {
 
       const expectedText = "GitHub Respositories Search";
       expect(headerElement.textContent).toBe(expectedText);
+    });
+
+    it("should render a search form", () => {
+      const { getByLabelText, getByText } = render(<SearchForm />);
+
+      const inputField = getByLabelText(/keywords/i);
+      expect(inputField).toBeTruthy();
+
+      const sortDropdown = getByLabelText(/sort/i);
+      expect(sortDropdown).toBeTruthy();
+
+      const orderDropdown = getByLabelText(/order/i);
+      expect(orderDropdown).toBeTruthy();
+
+      const searchButton = getByText(/^search$/i);
+      expect(searchButton).toBeTruthy();
+      expect(searchButton.textContent).toBe("Search");
     });
   });
 });
